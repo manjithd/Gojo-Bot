eff557fe25dfd08c467be17433e83ef845244af6
kimstuti123 <105543313+kimstuti123@users.noreply.github.com>
2022-05-14 20:59:28 +0530

    Update Enable.ts

--- src/commands/Dev/Enable.ts
+++ src/commands/Dev/Enable.ts
@@ -18,12 +18,12 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
         const key = joined.toLowerCase().trim()
-        if (!key) return void (await M.reply(`My Darling please provide the command you want to enable`))
+        if (!key) return void (await M.reply(`My Darling please provide the command you want to enable for everyone`))
         const command = this.handler.commands.get(key) || this.handler.aliases.get(key)
         if (!command) return void (await M.reply(`No command found`))
         if (!(await this.client.DB.disabledcommands.findOne({ command: command.config.command })))
             return void M.reply(`${this.client.util.capitalize(command.config.command)} is already enabled`)
         await this.client.DB.disabledcommands.deleteOne({ command: command.config.command })
-        await M.reply(`*${this.client.util.capitalize(command.config.command)}* is now Enabled`)
+        await M.reply(`*${this.client.util.capitalize(command.config.command)}* is now Enabled successfully`)
     }
 }
