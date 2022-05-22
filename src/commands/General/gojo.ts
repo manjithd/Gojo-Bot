375ba7764e5bb21d5f5f7627aa3cfe4a4c499aef
kimstuti123 <105543313+kimstuti123@users.noreply.github.com>
2022-05-22 22:17:53 +0530

    Update and rename tour bot.ts to gojo.ts

--- src/commands/General/gojo.ts
+++ src/commands/General/gojo.ts
@@ -9,7 +9,7 @@ import { ISimplifiedMessage } from "../../typings";
 export default class Command extends BaseCommand {
 	constructor(client: WAClient, handler: MessageHandler) {
 		super(client, handler, {
-			command: "Tour Bot",
+			command: "gojo",
 			description: "Displays the info",
 			category: "general",
 			usage: `${client.config.prefix}yotsuba`,
@@ -27,7 +27,7 @@ export default class Command extends BaseCommand {
 			{
 				quoted: M.WAMessage,
 				mimetype: Mimetype.gif,
-				caption: `📍 *Tour Bot* 📍\n\n🍀 *Description: A WhatsApp Bot With advanced features developed and hosted by Fantox & stuti.*\n\n🌐 *Fantox's GitHub URL: https://github.com/FantoX001* \n\n *Stuti's GitHub URL: https://github.com/kimstuti123* \n\n 👾 *Tour Bot's URL:https://github.com/FantoX001/Zero-Two* \n\n 📒  *Guide: https://github.com/FantoX001/Zero-Two-Guides* \n\n`,
+				caption: `⚡️ *Gojo Bot* ⚡️\n\n🍀 *Description: A WhatsApp Bot With advanced features developed and hosted by Fantox & stuti.*\n\n🌐 *Fantox's GitHub URL: https://github.com/FantoX001* \n\n *Stuti's GitHub URL: https://github.com/kimstuti123* \n\n 👾 *Tour Bot's URL:https://github.com/FantoX001/Zero-Two* \n\n 📒  *Guide: https://github.com/FantoX001/Zero-Two-Guides* \n\n`,
 			}
 		);
 	};
