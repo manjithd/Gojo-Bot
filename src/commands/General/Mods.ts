d2b076242347647dcaad299aaea57214a5ace57e
kimstuti123 <105543313+kimstuti123@users.noreply.github.com>
2022-05-22 22:19:19 +0530

    Update Mods.ts

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -24,7 +24,7 @@ export default class Command extends BaseCommand {
                     user.notify || user.vname || user.name || 'null'
                 }*\n🍀 *Contact: https://wa.me/+${user?.jid?.split('@')[0]}*\n\n`)
         )
-        text += `\n📍 Tour Bot📍 `
+        text += `\n⚡️ Gojo Bot ⚡️ `
         return void M.reply(text)
     }
 }
